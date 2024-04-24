09c8f9143e970dea591dc68539d0b7d937a91420
김주원 <sa6812000@naver.com>
2024-04-24 23:36:35 +0900

    save board

---
+++ demo/src/main/java/com/example/demo/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.BoardDTO;
+import com.example.demo.entity.BoardEntity;
+import com.example.demo.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public void save(BoardDTO boardDTO){
+        boardRepository.save(BoardEntity.toSaveEntity(boardDTO));
+    }
+}
